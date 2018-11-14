94465a6221c4651fa25caa7daa8b41c1155285cd
Jan Berge Ommedal <jan.ommedal@gmail.com>
2018-11-14 22:32:55 +0100

    implemented json logging provider

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,24 @@
+import logger from './logger'; // logging must be imported before unleash-server https://github.com/Unleash/unleash/blob/master/docs/getting-started.md#how-do-i-set-a-logger-provider
 import unleash from 'unleash-server';
 import enableAuth from './azuread-auth-hook';
 
 const DISABLE_AUTH = process.env.DISABLE_AUTH === 'true';
 const UNLEASH_PG_URL = process.env.UNLEASH_PG_URL;
 const UNLEASH_PG_USERNAME = process.env.UNLEASH_PG_USERNAME;
-const UNLEASH_PG_PASSWORD = process.env.UNLEASH_PG_PASSWORD;
-const dbUri = `postgres://${UNLEASH_PG_USERNAME}:${UNLEASH_PG_PASSWORD}@${UNLEASH_PG_URL!.slice(11, UNLEASH_PG_URL!.length)}`;
+const dbPassword = "" + process.env.UNLEASH_PG_PASSWORD;
+const dbUri = `postgres://${UNLEASH_PG_USERNAME}:${dbPassword}@${UNLEASH_PG_URL!.slice(11, UNLEASH_PG_URL!.length)}`;
 
-function logMaskedPassword(output: string, password: string) {
-    console.log(output.replace(password, "**********"));
+function maskPassword(output: string, password: string) {
+    return output.replace(password, "**********");
 }
 
-logMaskedPassword("connecting to database " + dbUri, UNLEASH_PG_PASSWORD!);
+logger.info(maskPassword("connecting to database " + dbUri, dbPassword!));
+
 
 unleash.start({
     databaseUrl: dbUri,
     port: 8080,
-    secret: UNLEASH_PG_PASSWORD,
+    secret: dbPassword,
     adminAuthentication: 'custom',
     preRouterHook: DISABLE_AUTH ? undefined : enableAuth
 });
