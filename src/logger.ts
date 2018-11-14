682150b83afab89289851e9ebca3e8891ff69719
Jan Berge Ommedal <jan.ommedal@gmail.com>
2018-11-14 22:32:55 +0100

    implemented json logging provider

---
+++ src/logger.ts
@@ -0,0 +1,31 @@
+import logger from 'unleash-server/lib/logger';
+
+function jsonLogger(level: string) {
+    return (message?: any, ...parameters: any[]) => {
+        console.log(JSON.stringify({
+            "@timestamp": new Date().toISOString(),
+            "@version": 1,
+            messsage: message,
+            level: level
+        }))
+    }
+}
+
+const infoLogger = jsonLogger("INFO");
+const debugLogger = jsonLogger("DEBUG");
+const warnLogger = jsonLogger("WARN");
+const errorLogger = jsonLogger("ERROR");
+
+function loggerProvider(name: any) {
+    infoLogger(`provinding logger: ${name}`);
+    return {
+        debug: debugLogger,
+        info: infoLogger,
+        warn: warnLogger,
+        error: errorLogger
+    };
+}
+
+logger.setLoggerProvider(loggerProvider);
+
+export default loggerProvider("default");
