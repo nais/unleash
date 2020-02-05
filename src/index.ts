e05148a63a1c46c378a060109bdbf710eef59a97
Christopher Kolstad <christopher.kolstad@nav.no>
2020-02-05 09:10:13 +0100

    Missing semicolon

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
 import logger from './logger';
 import unleash from 'unleash-server';
 import enableAuth from './azuread-auth-hook';
+const log = logger("default");
 
 const DISABLE_AUTH = process.env.DISABLE_AUTH === 'true';
 const UNLEASH_PG_URL = process.env.UNLEASH_PG_URL ? process.env.UNLEASH_PG_URL : "localhost";
 const UNLEASH_PG_USERNAME = process.env.UNLEASH_PG_USERNAME ? process.env.UNLEASH_PG_USERNAME : "unleash";
 const gcp = process.env.GCP_SQLINSTANCE_UNLEASH_DB_PASSWORD ? process.env.GCP_SQLINSTANCE_UNLEASH_DB_PASSWORD : "";
 const vaultP = process.env.UNLEASH_PG_PASSWORD ? process.env.UNLEASH_PG_PASSWORD : "";
-const dbPassword = "" + gcp + vaultP
+const dbPassword = "" + gcp + vaultP;
 const dbUri = `postgres://${UNLEASH_PG_USERNAME}:${dbPassword}@${UNLEASH_PG_URL!.slice(11, UNLEASH_PG_URL!.length)}`;
-const log = logger("default");
 function maskPassword(output: string, password: string) {
     return output.replace(password, "**********");
 }
