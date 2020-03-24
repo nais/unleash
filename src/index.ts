79fb7bc81f07bc4e96053222d45c3c78401fc6cf
Christopher Kolstad <christopher.kolstad@nav.no>
2020-03-24 08:46:22 +0100

    Legg til støtte for NAIS_DATABASE format

--- src/index.ts
+++ src/index.ts
@@ -4,17 +4,25 @@ import enableAuth from './azuread-auth-hook';
 const log = logger("default");
 
 const DISABLE_AUTH = process.env.DISABLE_AUTH === 'true';
-const UNLEASH_PG_URL = process.env.UNLEASH_PG_URL ? process.env.UNLEASH_PG_URL : "postgres://localhost/unleash";
-const UNLEASH_PG_USERNAME = process.env.UNLEASH_PG_USERNAME ? process.env.UNLEASH_PG_USERNAME : "unleash";
-const gcp = process.env.GCP_SQLINSTANCE_UNLEASH_DB_PASSWORD ? process.env.GCP_SQLINSTANCE_UNLEASH_DB_PASSWORD : "";
-const vaultP = process.env.UNLEASH_PG_PASSWORD ? process.env.UNLEASH_PG_PASSWORD : "";
-const dbPassword = "" + gcp + vaultP;
-const dbUri = `postgres://${UNLEASH_PG_USERNAME}:${dbPassword}@${UNLEASH_PG_URL!.slice(11, UNLEASH_PG_URL!.length)}`;
+const NAIS_PG_URL = process.env.NAIS_DATABASE_UNLEASH_UNLEASH_DB_URL ? process.env.NAIS_DATABASE_UNLEASH_UNLEASH_DB_URL : "";
+let dbUri = "";
+let dbPassword = "";
+if (NAIS_PG_URL != "") {
+    dbUri = NAIS_PG_URL;
+    dbPassword = process.env.NAIS_DATABASE_UNLEASH_UNLEASH_DB_PASSWORD ? process.env.NAIS_DATABASE_UNLEASH_UNLEASH_DB_PASSWORD : "";
+} else {
+    const UNLEASH_PG_URL = process.env.UNLEASH_PG_URL ? process.env.UNLEASH_PG_URL : "postgres://localhost/unleash";
+    const UNLEASH_PG_USERNAME = process.env.UNLEASH_PG_USERNAME ? process.env.UNLEASH_PG_USERNAME : "unleash";
+    const gcp = process.env.GCP_SQLINSTANCE_UNLEASH_DB_PASSWORD ? process.env.GCP_SQLINSTANCE_UNLEASH_DB_PASSWORD : "";
+    const vaultP = process.env.UNLEASH_PG_PASSWORD ? process.env.UNLEASH_PG_PASSWORD : "";
+    dbPassword = "" + gcp + vaultP;
+    dbUri = `postgres://${UNLEASH_PG_USERNAME}:${dbPassword}@${UNLEASH_PG_URL!.slice(11, UNLEASH_PG_URL!.length)}`;
+}
+log.info(maskPassword("connecting to database " + dbUri, dbPassword!));
 function maskPassword(output: string, password: string) {
     return output.replace(password, "**********");
 }
 
-log.info(maskPassword("connecting to database " + dbUri, dbPassword!));
 
 unleash.start({
     databaseUrl: dbUri,
