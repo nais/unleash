a310a11963820bc36967275f1902e1dcde3145d5
Kyrre Havik Eriksen <kyrremann@gmail.com>
2020-09-24 16:17:07 +0200

    byttet til log.info

--- src/azuread-auth-hook.ts
+++ src/azuread-auth-hook.ts
@@ -8,7 +8,7 @@ import { config } from "./azuread-configuration";
 import {User} from "./models";
 
 function enableAzureAd(app: Application) {
-    console.log(config.redirectUrl);
+    log.info(config.redirectUrl);
     passport.use(new OIDCStrategy(config,
         function(profile, done) {
             if (!profile.oid) {
