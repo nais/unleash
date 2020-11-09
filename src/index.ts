a46426711723b09db70c78474085671e60c071f2
Kyrre Havik Eriksen <kyrremann@gmail.com>
2020-11-09 09:07:09 +0100

    forenklet DB-tilkobling, ingen vault lengre

--- src/index.ts
+++ src/index.ts
@@ -4,36 +4,17 @@ import enableAuth from './azuread-auth-hook';
 const log = logger("default");
 
 const DISABLE_AUTH = process.env.DISABLE_AUTH === 'true';
-const NAIS_PG_URL = process.env.SQL_URL ? process.env.SQL_URL : "";
-const NAIS_USER = process.env.SQL_USERNAME ? process.env.SQL_USERNAME : "";
-log.info("Found NAIS_USER " + NAIS_USER);
-let dbUri = "";
-let dbPassword = "";
-if (NAIS_PG_URL != "") {
-    dbUri = NAIS_PG_URL;
-    dbPassword = process.env.SQL_PASSWORD ? process.env.SQL_PASSWORD : "";
-    log.info("DB Injected from GCP");
-} else {
-    const UNLEASH_PG_URL = process.env.UNLEASH_PG_URL ? process.env.UNLEASH_PG_URL : "postgres://localhost/unleash";
-    const UNLEASH_PG_USERNAME = process.env.UNLEASH_PG_USERNAME ? process.env.UNLEASH_PG_USERNAME : "unleash";
-    const gcp = process.env.GCP_SQLINSTANCE_UNLEASH_DB_PASSWORD ? process.env.GCP_SQLINSTANCE_UNLEASH_DB_PASSWORD : "";
-    const vaultP = process.env.UNLEASH_PG_PASSWORD ? process.env.UNLEASH_PG_PASSWORD : "";
-    dbPassword = "" + gcp + vaultP;
-    dbUri = `postgres://${UNLEASH_PG_USERNAME}:${dbPassword}@${UNLEASH_PG_URL!.slice(11, UNLEASH_PG_URL!.length)}`;
-    log.info("Running with old setup/Vault");
-}
-log.info(maskPassword("connecting to database " + dbUri, dbPassword!));
-function maskPassword(output: string, password: string) {
-    return output.replace(password, "**********");
-}
+const DB_URI = process.env.DB_URL ? process.env.DB_URL : "postgres://postgres/unleash";
+const DB_PASSWORD = process.env.DB_PASSWORD ? process.env.DB_PASSWORD : "unleash";
 
+log.info("connecting to database " + DB_URI);
 
 unleash.start({
-    databaseUrl: dbUri,
+    databaseUrl: DB_URI,
     poolMin: 2,
     poolMax: 6,
     port: 8080,
-    secret: dbPassword,
+    secret: DB_PASSWORD,
     adminAuthentication: 'custom',
     preRouterHook: DISABLE_AUTH ? undefined : enableAuth,
     getLogger: logger
